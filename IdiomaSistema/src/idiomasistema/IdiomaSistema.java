a2cd6e37332efda818d8421bba690a34747763dc
Samuel <samugit.projects@gmail.com>
2023-11-07 12:22:51 -0300

    Aula04 Exercício 02

---
+++ IdiomaSistema/src/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,14 @@
+
+package idiomasistema;
+
+import java.util.Locale;
+
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        
+        Locale idioma = Locale.getDefault();
+        System.out.println("O idioma do sistema é " + idioma.getDisplayLanguage());
+    }
+    
+}
