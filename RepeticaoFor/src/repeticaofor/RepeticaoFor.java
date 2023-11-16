04a41191e406dd77a70addc7fc67791259566659
Samuel <samugit.projects@gmail.com>
2023-11-16 15:30:16 -0300

    Aula13 Estruturas de Repetição (Parte 3)

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author samuel
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=0; cc<=100; cc+=10){
+            System.out.println(cc);
+        }
+        /*
+        int cc = 0;
+        while (cc<4){
+          System.out.println("Cambalhota");
+          c++;
+        }
+        */
+    }
+    
+}
