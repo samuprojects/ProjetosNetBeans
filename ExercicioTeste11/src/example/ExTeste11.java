498f38243291d815458bd7c6bbdca34d92d61132
Samuel <samugit.projects@gmail.com>
2023-11-15 22:04:13 -0300

    Aula11 Exercício Cálculo do Fatorial

---
+++ ExercicioTeste11/src/example/ExTeste11.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example;
+
+/**
+ *
+ * @author samuel
+ */
+public class ExTeste11 {
+
+    public static void main(String[] args) {
+            
+    char c = 'c';
+    int i  = 10;
+    double d = 10;
+    long l = 1;
+    String s = "Hello";
+        
+    // CORRETO i += l;
+    // ERRO c += s;
+    // ERRO i += s;
+    // CORRETA s += i;
+    // ERRO c = c + i;
+    }
+}
