c60af6a72e90441980494775205b3f473d1737ad
Samuel <samugit.projects@gmail.com>
2023-11-17 15:13:28 -0300

    Encerrando teste

---
+++ Testes16/src/testes16/Testes16.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testes16;
+
+/**
+ *
+ * @author samuel
+ */
+public class Testes16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+      /*  int v0 = 3;
+        int v1 = v0++;
+        int v2 = ++v1;
+        v1 += v0;
+        v2 += --v1;
+
+        System.out.println(v0);
+        System.out.println("");
+        System.out.println(v1);
+        System.out.println("");
+        System.out.println(v2);
+        
+        for (int i = 1; i < 5; i++) {
+	if (i==2) continue;
+	System.out.print("i = " + i); 
+*/
+    /*  
+      int total = 0;
+int c[] = new int[13];
+for (int i = 0; i<c.length; i++) {
+	c[i] = i + i;
+}
+for (int i = 0; i<c.length; i++) {
+	total += c[i];
+}
+System.out.println(total);
+}
+*/
+    
+    /*
+    Object[] str = {"a", "ab", "abc"};
+        System.out.println(str[1]);
+*/
+    
+        int x, y;
+        double z;
+
+        x = 5;
+        y = 2;
+        z = x / y;
+        System.out.print(z);
+
+    }
+}
+
+
